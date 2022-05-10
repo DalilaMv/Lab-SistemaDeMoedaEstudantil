499192107672961d0cbb6916cda06feb987021c8
Dalila <dalilamv99@outlook.com>
2022-05-09 23:56:50 -0300

    add models de empresa e vantagens

---
+++ implementação/backend/src/empresa/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+
+# Create your models here.
+        
+class Empresa(models.Model):
+    nome = models.CharField(max_length=300)
+    cnpj = models.CharField(max_length=11, unique=True)
+    
+    class Meta:
+        db_table = 'empresa'
+    
+class Vantagem(models.Model):
+    descricao = models.TextField()
+    empresa = models.ForeignKey(Empresa, on_delete=models.CASCADE)
+    
+    class Meta:
+        db_table = 'vantagem'
