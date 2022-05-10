2ca0a74b1bf8387433dba568e543274951ab6eda
Dalila <dalilamv99@outlook.com>
2022-05-10 13:58:29 -0300

    add empresa viewset

--- implementação/backend/src/empresa/api/viewsets.py
+++ implementação/backend/src/empresa/api/viewsets.py
@@ -1,10 +1,23 @@
 from rest_framework.viewsets import ModelViewSet
+from rest_framework.response import Response
 from empresa.models import *
 from empresa.api.serializers import *
-
+from rest_framework.decorators import action
 
 class EmpresaViewSet(ModelViewSet):
     serializer_class = EmpresaSerializer
     queryset = Empresa.objects.all()
     pagination_class = None
     
+    @action(methods=['post'], detail=False)
+    def save(self, request, *args, **kwargs):
+        
+    
+        user = User.objects.create(username=request.data["username"],
+                                password=request.data["password"]).save()
+        
+        empresa = Empresa.objects.create(nome=request.data["nome"],
+                                    cnpj=request.data["cnpj"], 
+                                    user_id=user.id).save()
+    
+        return Response(empresa)
--- implementação/backend/src/pessoa/api/viewsets.py
+++ implementação/backend/src/pessoa/api/viewsets.py
@@ -2,6 +2,7 @@ from rest_framework.viewsets import ModelViewSet
 from pessoa.models import *
 from pessoa.api.serializers import *
 from rest_framework.decorators import action
+from rest_framework.response import Response
 
 
 class PessoaViewSet(ModelViewSet):
@@ -18,20 +19,16 @@ class AlunoViewSet(ModelViewSet):
     @action(methods=['post'], detail=False)
     def save(self, request, *args, **kwargs):
         request.data["username"]
-        request.data["password"]
-        
         user = User.objects.create(username=request.data["username"],
                                 password=request.data["password"]).save()
-        
         pessoa = Pessoa.objects.create(nome=request.data["nome"],
                                     instituicao_id=request.data["instituicao"],
                                     cpf=request.data["cpf"], 
                                     user_id=user.id).save()
-        
-        Aluno.objects.create(pessoa_id=pessoa,
+        aluno = Aluno.objects.create(pessoa_id=pessoa,
                         rg=request.data["rg"],
                         curso=request.data["curso"]).save()
 
-        return 
+        return Response(aluno)
 
 
