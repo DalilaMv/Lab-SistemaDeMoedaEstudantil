17be2bd1ed1e3183dfe2c1aa347278f90a69a9c2
Dalila <dalilamv99@outlook.com>
2022-05-10 13:38:34 -0300

    add field user to empresa

--- implementação/backend/src/empresa/models.py
+++ implementação/backend/src/empresa/models.py
@@ -1,10 +1,12 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
         
 class Empresa(models.Model):
     nome = models.CharField(max_length=300)
     cnpj = models.CharField(max_length=12, unique=True)
+    user = models.ForeignKey(User, models.PROTECT, null=True, blank=True)
     
     class Meta:
         db_table = 'empresa'
