adf202799d7c9b91fa94fb4e41eb4b3c7fd7f8ef
Dalila <dalilamv99@outlook.com>
2022-05-24 12:49:05 -0300

    add rest_framework auth

--- implementação/backend/src/app/settings.py
+++ implementação/backend/src/app/settings.py
@@ -28,6 +28,16 @@ DEBUG = True
 ALLOWED_HOSTS = []
 
 
+REST_FRAMEWORK = {
+    'DEFAULT_PERMISSION_CLASSES': (
+        'rest_framework.permissions.IsAuthenticated',
+    ),
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework.authentication.SessionAuthentication',
+        'rest_framework.authentication.TokenAuthentication',
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    ),
+}
 # Application definition
 
 INSTALLED_APPS = [
